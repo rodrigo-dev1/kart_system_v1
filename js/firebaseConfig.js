c657d9591ff0090a92b78c4fbb62d828a362e5e6
rodrigo-dev1 <cruz.rodrigo.rcs@gmail.com>
2026-04-29 23:56:58 -0300

    ajuste credencial

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "PREENCHER_COM_API_KEY_WEB",
+  apiKey: "AIzaSyC_ruvtoN9KFp9K4cuJeL17Z_KVN9tTO5s",
   authDomain: "kart-v1.firebaseapp.com",
   projectId: "kart-v1",
-  storageBucket: "kart-v1.appspot.com",
-  messagingSenderId: "PREENCHER",
-  appId: "PREENCHER"
+  storageBucket: "kart-v1.firebasestorage.app",
+  messagingSenderId: "524238423587",
+  appId: "1:524238423587:web:39d9d17963b4ee59ef5396"
 };
 
 firebase.initializeApp(firebaseConfig);
